77b19e5003ed4c2868c8c75a1e7c645cbd5a945a
Ivo <ivorosenberger@gmail.com>
2022-07-27 21:12:36 -0300

    finalizado de carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,136 +1,260 @@
-const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
-addToShoppingCartButtons.forEach((addToCartButton) => {
-  addToCartButton.addEventListener('click', addToCartClicked);
-});
-
-const comprarButton = document.querySelector('.comprarButton');
-comprarButton.addEventListener('click', comprarButtonClicked);
-
-const shoppingCartItemsContainer = document.querySelector(
-  '.shoppingCartItemsContainer'
-);
-
-function addToCartClicked(event) {
-  const button = event.target;
-  const item = button.closest('.iteamAll');
-
-  const itemTitle = document.querySelector('.item-title').textContent;
-  const itemPrice = document.querySelector('.item-price').textContent;
-  const itemImage = document.querySelector('.item-image').src;
-
-  addItemToShoppingCart(itemTitle, itemPrice, itemImage);
-}
-
-function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
-  const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
-    'shoppingCartItemTitle'
-  );
-  for (let i = 0; i < elementsTitle.length; i++) {
-    if (elementsTitle[i].innerText === itemTitle) {
-      let elementQuantity = elementsTitle[
-        i
-      ].parentElement.parentElement.parentElement.querySelector(
-        '.shoppingCartItemQuantity'
-      );
-      elementQuantity.value++;
-      toast();
-      updateShoppingCartTotal();
-      return;
+document.addEventListener('DOMContentLoaded', () => {
+  const baseDeDatos = [
+    {
+      id: 1,
+      nombre: "Buzo Triple-K",
+      precio: 4900,
+      imagen: "../images/buzo.jpg"
+    },
+    {
+      id: 2,
+      nombre: "Buzo Urban",
+      precio: 4500,
+      imagen: "../images/buzo5.jpg"
+    },
+    {
+      id: 3,
+      nombre: "Buzo Tacuara",
+      precio: 4700,
+      imagen: "../images/buzo3.jpg",
+    },
+    {
+      id: 4,
+      nombre: "Pantalon Flower",
+      precio: 3900,
+      imagen: "../images/pantalones.jpg"
+    },
+    {
+      id: 5,
+      nombre: "Buzo BSAS-K",
+      precio: 4900,
+      imagen: "../images/buzo bsas.jpg"
+    },
+    {
+      id: 6,
+      nombre: "Buzo Colorinche",
+      precio: 4500,
+      imagen: "../images/Buzos.jpg"
+    },
+    {
+      id: 7,
+      nombre: "Buzo Amarello",
+      precio: 5700,
+      imagen: "../images/buzoama.jpg"
+    },
+    {
+      id: 8,
+      nombre: "Campera Oslo KOK",
+      precio: 5200,
+      imagen: "../images/campera1.jpg"
+    },
+    {
+      id: 9,
+      nombre: "Buzo Cruel",
+      precio: 4600,
+      imagen: "../images/buzoblanconegro.jpg"
+    },
+    {
+      id: 10,
+      nombre: "Buzo Pista",
+      precio: 4500,
+      imagen: "../images/buzoblue.jpg"
+    },
+    {
+      id: 11,
+      nombre: "Buzo Bielsa",
+      precio: 4700,
+      imagen: "../images/buzoloco.jpg"
+    },
+    {
+      id: 12,
+      nombre: "Coleccion Pela",
+      precio: 7800,
+      imagen: "../images/colecciones.jpg"
+    },
+    {
+      id: 13,
+      nombre: "Pantalon Black-k",
+      precio: 4500,
+      imagen: "../images/pantalon.jpg"
+    },
+    {
+      id: 14,
+      nombre: "Remera Urban-k",
+      precio: 4700,
+      imagen: "../images/urban.jpg"
+    },
+    {
+      id: 15,
+      nombre: "Coleccion Facha",
+      precio: 8900,
+      imagen: "../images/buzo2.jpg"
     }
+  ];
+
+  // Variables
+
+  let carrito = [];
+  const divisa = '$';
+  const DOMitems = document.querySelector('#items');
+  const DOMcarrito = document.querySelector('#carrito');
+  const DOMtotal = document.querySelector('#total');
+  const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+  const miLocalStorage = window.localStorage;
+
+  // Funciones
+  function renderizarProductos() {
+    baseDeDatos.forEach((info) => {
+      const miNodo = document.createElement('div');
+      miNodo.classList.add('card', 'col-sm-4');
+      const miNodoCardBody = document.createElement('div');
+      miNodoCardBody.classList.add('card-body');
+      const miNodoTitle = document.createElement('h5');
+      miNodoTitle.classList.add('card-title');
+      miNodoTitle.textContent = info.nombre;
+      const miNodoImagen = document.createElement('img');
+      miNodoImagen.classList.add('img-fluid');
+      miNodoImagen.setAttribute('src', info.imagen);
+      const miNodoPrecio = document.createElement('p');
+      miNodoPrecio.classList.add('card-text');
+      miNodoPrecio.textContent = `${info.precio}${divisa}`;
+      const miNodoBoton = document.createElement('button');
+      miNodoBoton.classList.add('btn', 'btn-primary');
+      miNodoBoton.textContent = '+';
+      miNodoBoton.setAttribute('marcador', info.id);
+      miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+      // Insertamos
+      miNodoCardBody.appendChild(miNodoImagen);
+      miNodoCardBody.appendChild(miNodoTitle);
+      miNodoCardBody.appendChild(miNodoPrecio);
+      miNodoCardBody.appendChild(miNodoBoton);
+      miNodo.appendChild(miNodoCardBody);
+      DOMitems.appendChild(miNodo);
+    });
   }
 
 
+  /*  Evento para añadir un producto al carrito de la compra */
+
+  function anyadirProductoAlCarrito(evento) {
+    carrito.push(evento.target.getAttribute('marcador'))
+    // Actualizamos el carrito 
+    renderizarCarrito();
+    // Actualizamos el LocalStorage
+    guardarCarritoEnLocalStorage();
+  }
+
+  /* Dibuja todos los productos guardados en el carrito */
+
+  function renderizarCarrito() {
+    // Vaciamos todo el html
+    DOMcarrito.textContent = '';
+    // Quitamos los duplicados
+    const carritoSinDuplicados = [...new Set(carrito)];
+    // Generamos los Nodos a partir de carrito
+    carritoSinDuplicados.forEach((item) => {
+      // Obtenemos el item que necesitamos de la variable base de datos
+      const miItem = baseDeDatos.filter((itemBaseDatos) => {
+        // ¿Coincide las id? Solo puede existir un caso
+        return itemBaseDatos.id === parseInt(item);
+      });
+      // Cuenta el número de veces que se repite el producto
+      const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+        // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+        return itemId === item ? total += 1 : total;
+      }, 0);
+      // Creamos el nodo del item del carrito
+      const miNodo = document.createElement('li');
+      miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+      miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+      // Boton de borrar
+      const miBoton = document.createElement('button');
+      miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+      miBoton.textContent = 'X';
+      miBoton.style.marginLeft = '1rem';
+      miBoton.dataset.item = item;
+      miBoton.addEventListener('click', borrarItemCarrito);
+      // Mezclamos nodos
+      miNodo.appendChild(miBoton);
+      DOMcarrito.appendChild(miNodo);
+    });
+    // Renderizamos el precio total en el HTML
+    DOMtotal.textContent = calcularTotal();
+  }
+
+  /* Evento para borrar un elemento del carrito */
+
+  function borrarItemCarrito(evento) {
+    // Obtenemos el producto ID que hay en el boton pulsado
+    const id = evento.target.dataset.item;
+    // Borramos todos los productos
+    carrito = carrito.filter((carritoId) => {
+      return carritoId !== id;
+    });
+    // volvemos a renderizar
+    renderizarCarrito();
+    // Actualizamos el LocalStorage
+    guardarCarritoEnLocalStorage();
+
+  }
 
 
+  /* Calcula el precio total teniendo en cuenta los productos repetidos */
+  function calcularTotal() {
+    // Recorremos el array del carrito 
+    return carrito.reduce((total, item) => {
+      // De cada elemento obtenemos su precio
+      const miItem = baseDeDatos.filter((itemBaseDatos) => {
+        return itemBaseDatos.id === parseInt(item);
+      });
+      // Los sumamos al total
+      return total + miItem[0].precio;
+    }, 0).toFixed(2);
+  }
 
-  const shoppingCartRow = document.createElement('div');
-  const shoppingCartContent = `
-  <div class="row shoppingCartItem">
-        <div class="col-6">
-            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <img src=${itemImage} class="shopping-cart-image">
-                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
-            </div>
-        </div>
-        <div class="col-2">
-            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
-            </div>
-        </div>
-        <div class="col-4">
-            <div
-                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                    value="1">
-                <button class="btn btn-danger buttonDelete" type="button">X</button>
-            </div>
-        </div>
-    </div>`;
-  shoppingCartRow.innerHTML = shoppingCartContent;
-  shoppingCartItemsContainer.append(shoppingCartRow);
 
-  shoppingCartRow
-    .querySelector('.buttonDelete')
-    .addEventListener('click', removeShoppingCartItem);
+  /* Varia el carrito y vuelve a dibujarlo */
 
-  shoppingCartRow
-    .querySelector('.shoppingCartItemQuantity')
-    .addEventListener('change', quantityChanged);
+  function vaciarCarrito() {
+    // Limpiamos los productos guardados
+    carrito = [];
+    // Renderizamos los cambios
+    renderizarCarrito();
+    // Borra LocalStorage
+    localStorage.clear();
 
-  updateShoppingCartTotal();
+  }
+
+  function guardarCarritoEnLocalStorage() {
+    miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+  }
+
+  function cargarCarritoDeLocalStorage() {
+    // ¿Existe un carrito previo guardado en LocalStorage?
+    if (miLocalStorage.getItem('carrito') !== null) {
+      // Carga la información
+      carrito = JSON.parse(miLocalStorage.getItem('carrito'));
+    }
+  }
+
+  // Eventos
+  DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+  // Inicio
+  cargarCarritoDeLocalStorage();
+  renderizarProductos();
+  renderizarCarrito();
+});
+
+const API = "../products.json";
+
+const getProducts = async () => {
+
+  const respuesta = await fetch(API);
+
+  products = await respuesta.json();
+
+  console.log(products);
 }
-
-function updateShoppingCartTotal() {
-  let total = 0;
-  const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
-
-  const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
-
-  shoppingCartItems.forEach((shoppingCartItem) => {
-    const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
-      '.shoppingCartItemPrice'
-    );
-    const shoppingCartItemPrice = Number(
-      shoppingCartItemPriceElement.textContent.replace('$', '')
-    );
-    const shoppingCartItemQuantityElement = shoppingCartItem.querySelector(
-      '.shoppingCartItemQuantity'
-    );
-    const shoppingCartItemQuantity = Number(
-      shoppingCartItemQuantityElement.value
-    );
-    total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
-  });
-  shoppingCartTotal.innerHTML = `${total.toFixed(2)}$`;
-}
-
-function removeShoppingCartItem(event) {
-  const buttonClicked = event.target;
-  buttonClicked.closest('.shoppingCartItem').remove();
-  updateShoppingCartTotal();
-}
-
-function quantityChanged(event) {
-  const input = event.target;
-  input.value <= 0 ? (input.value = 1) : null;
-  updateShoppingCartTotal();
-}
-
-function comprarButtonClicked() {
-  shoppingCartItemsContainer.innerHTML = '';
-  updateShoppingCartTotal();
-}
-
-
-function toast(){
-  const btnTostada = document.querySelector('.addToCart')
-btnTostada.addEventListener('click', () => {
-  Toastify({
-    text: "Agregar al carrito perfectamente",
-    duration: 3000 , 
-    gravity: "bottom" , 
-    position: "right"
-  })
-}).showToast();
-}
+getProducts();
 
